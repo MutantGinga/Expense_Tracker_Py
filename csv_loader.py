844989b8c5daab66de0fa59da9275176bbc6a6ea
MutantGinga <haydenwilliamhefford@yahoo.com>
2025-10-04 00:01:30 +0100

    Tried various file formats for the statement
    cannot seem to find correct file format for the statement so that it can be read by Pandas. Need to continue experimenting. Currently, all formats seem to result in lots of unnamed columns with scatterings of missing data.

--- csv_loader.py
+++ csv_loader.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('Statements_Midata.csv', encoding="cp1252", sep=";")
-print(df.to_string())
+df = pd.read_csv('Statement.csv', encoding="cp1252")
+#df = pd.read_excel('Statement.xlsx')
+print(df.isnull())
+print(df)
